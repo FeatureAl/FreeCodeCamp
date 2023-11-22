d947c6c55d1cd9dfb753ec0bbb2f37e32584cead
FeatureAl <alenfront@gmail.com>
2023-11-23 00:35:03 +0700

    +=

---
+++ Basic_JS.js
@@ -0,0 +1,18 @@
+//Finding a Remainder in JavaScript
+// description
+//Set remainder equal to the remainder of 11 divided by 3 using the remainder (%) operator.
+
+// solution
+const remainder = 11%3;
+
+
+//Compound Assignment With Augmented Addition
+// description
+//let a = 3;
+//let b = 17;
+//let c = 12;
+
+// solution
+a += 12;
+b += 9;
+c += 7;
--- Finding-a-Remainder-in-JavaScript.js
+++
@@ -1,5 +0,0 @@
-// description
-//Set remainder equal to the remainder of 11 divided by 3 using the remainder (%) operator.
-
-// solution
-const remainder = 11%3;
