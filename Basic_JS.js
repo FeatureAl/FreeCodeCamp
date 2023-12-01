fc470a5e01b36f949d7d52b487b738965bba7819
FeatureAl <alenfront@gmail.com>
2023-12-01 18:12:29 +0700

    49

--- Basic_JS.js
+++ Basic_JS.js
@@ -111,3 +111,16 @@ const myArray = [
 // solution
 const myList = [["squash", 7], ["turkey", 2], ["cucumbers", 5], ["banana", 6], ["Sweet things", 12]];
 
+
+
+
+  // description
+//Create a function called functionWithArgs that accepts two arguments and outputs their sum to the dev console.
+//Call the function with two numbers as arguments.
+
+// solution
+function functionWithArgs(a, b) {
+  console.log(a + b);
+  }
+functionWithArgs(1,2);
+functionWithArgs(7,9);
